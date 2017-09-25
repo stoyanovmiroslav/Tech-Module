f39047ebff83778f8e4a82ceaea7617da379d6b9
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2017-09-25 23:17:12 +0300

    Upload

---
+++ Programming Fundamentals/Conditional Statements and Loops  - Exercises/05. WordinPlural/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            if (name.EndsWith("o") || name.EndsWith("ch") || name.EndsWith("s") || name.EndsWith("sh")
+                || name.EndsWith("x") || name.EndsWith("z"))
+            {
+                Console.WriteLine(name + "es");
+            }
+            else if (name.EndsWith("y"))
+            {
+                name = name.Remove(name.Length - 1);
+                Console.WriteLine(name + "ies");
+            }
+            else
+            {
+                Console.WriteLine(name + "s");
+            }
+
+        }
+    }
+}
+
