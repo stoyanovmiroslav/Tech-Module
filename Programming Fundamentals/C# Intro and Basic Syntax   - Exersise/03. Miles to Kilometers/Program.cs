ee2850406357e28415c4d0894a0503368264898a
Miroslav Soyanov <32163254+stoyanovmiroslav@users.noreply.github.com>
2017-09-26 16:47:00 +0300

    Update Homework

--- Programming Fundamentals/C# Intro and Basic Syntax   - Exersise/03. Miles to Kilometers/Program.cs
+++ Programming Fundamentals/C# Intro and Basic Syntax   - Exersise/03. Miles to Kilometers/Program.cs
@@ -10,18 +10,10 @@ namespace _03.Miles_to_Kilometers
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int volume = int.Parse(Console.ReadLine());
-            int energy = int.Parse(Console.ReadLine());
-            int sugar = int.Parse(Console.ReadLine());
+           double miles = double.Parse(Console.ReadLine());
 
-            var energyfinal = energy * volume;
-            var sugarfinal = sugar * volume / 100;
-
-            Console.WriteLine($"{energy}   {sugar}");
-
-
-
+            double kilometers = miles * 1.60934;
+            Console.WriteLine($"{kilometers:F2}");
         }
     }
 }
