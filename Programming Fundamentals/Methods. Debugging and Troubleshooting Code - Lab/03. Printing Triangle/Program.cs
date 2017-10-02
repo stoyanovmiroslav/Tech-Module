9091dd701c5f5c2f357001b081a4e7aedf88c7a3
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2017-10-02 22:17:07 +0300

    Homework

---
+++ Programming Fundamentals/Methods. Debugging and Troubleshooting Code - Lab/03. Printing Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Programming Fundamentals/Methods. Debugging and Troubleshooting Code - Lab/04. Draw a Filled Square/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Draw_a_Filled_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
