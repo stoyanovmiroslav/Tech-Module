0fa385c133c7c4240df243a63dc628465fcfba2b
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2017-10-02 21:10:44 +0300

    Homework

--- Programming Fundamentals/Data Types and Variables - Exercises/15. FastPrimeChecker/Program.cs
+++ Programming Fundamentals/Data Types and Variables - Exercises/15. FastPrimeChecker/Program.cs
@@ -10,11 +10,20 @@ namespace _15.FastPrimeChecker
     {
         static void Main(string[] args)
         {
-
-            int num = 100;
-            TimeSpan span = new TimeSpan(int seconds, num);
-            Console.WriteLine(span);
-           
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= number; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime}");
+            }
         }
     }
 }
