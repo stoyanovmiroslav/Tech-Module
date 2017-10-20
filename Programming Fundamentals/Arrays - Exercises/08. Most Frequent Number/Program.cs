3d327ecbce9d3ec8f144d6c3f9ff29de4ec424a8
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2017-10-20 21:00:20 +0300

    Homework

---
+++ Programming Fundamentals/Arrays - Exercises/08. Most Frequent Number/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;		
+using System.Collections.Generic;		
+using System.Linq;		
+using System.Text;		
+using System.Threading.Tasks;		
+		
+namespace _08.Most_Frequent_Number
+{		
+    class Program
+    {		
+        static void Main(string[] args)
+        {		
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();		
+		
+            int count = 0;		
+            int currentCount = 0;		
+            int maxNumber = 0;		
+		
+            for (int i = 0; i<numbers.Length ; i++)		
+            {		
+                count = 0;		
+                for (int j = i + 1; j<numbers.Length; j++)		
+                {		
+                    if (numbers[i] == numbers[j])		
+                    {		
+                        count++;		
+		
+                        if (count > currentCount)		
+                        {		
+                            currentCount = count;		
+                            maxNumber = numbers[i];		
+                        }		
+                    }		
+                }		
+            }		
+		
+            if (currentCount > 0)		
+            {		
+                Console.WriteLine(maxNumber);		
+            }		
+            else		
+            {		
+                Console.WriteLine(numbers[0]);		
+            }		
+        }		
+    }		
+}
--- Programming Fundamentals/Objects and Classes - Lab/02. Randomize Words/Program.cs
+++ Programming Fundamentals/Objects and Classes - Lab/02. Randomize Words/Program.cs
@@ -10,6 +10,22 @@ namespace _02.Randomize_Words
     {
         static void Main(string[] args)
         {
+            string[] words = Console.ReadLine()
+                            .Split(' ');
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+             int firstIndex = rnd.Next(0, words.Length);
+             string reminder = words[firstIndex];
+
+             int secondIndex = rnd.Next(0, words.Length);
+             words[firstIndex] = words[secondIndex];
+             words[secondIndex] = reminder; 
+            }
+
+            Console.WriteLine(string.Join("\r\n", words));
+
         }
     }
 }
