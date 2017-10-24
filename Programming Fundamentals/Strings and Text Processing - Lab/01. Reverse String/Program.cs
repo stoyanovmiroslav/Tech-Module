401fe2566c80c9f5dac827576b55dab8e7d34cce
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2017-10-24 12:36:32 +0300

    Homework

---
+++ Programming Fundamentals/Strings and Text Processing - Lab/01. Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToCharArray().Reverse();
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
---
+++ Programming Fundamentals/Strings and Text Processing - Lab/04. Palindromes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ', '.', ',', '?', '!' } , StringSplitOptions.RemoveEmptyEntries);
+            List<String> listOfPalindromes = new List<String>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+               bool isTrueOrFalse = IsPalindrome(text[i]);
+                if (isTrueOrFalse)
+                {
+                    listOfPalindromes.Add(text[i]);
+                }
+            }
+
+            listOfPalindromes = listOfPalindromes.OrderBy(x => x).Distinct().ToList();
+            Console.WriteLine(string.Join(", ", listOfPalindromes));
+
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            string reversed = new string(word.Reverse().ToArray());
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
