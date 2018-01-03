370be77f62bd1162103fd94f6059458d904ef0e1
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2018-01-03 18:57:29 +0200

    Software Technologies - examp

---
+++ Software Technologies/Software Technologies Exams/TODOList/C# Skeleton/TodoList/Controllers/TaskController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+        [ValidateInput(false)]
+	public class TaskController : Controller
+	{
+        private TodoListDbContext db = new TodoListDbContext();
+
+        [HttpGet]
+        [Route("")]
+	    public ActionResult Index()
+	    {
+	         var tasks = db.Tasks.ToList();
+            return View(tasks);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+		{
+            return View(new Task());
+        }
+
+		[HttpPost]
+		[Route("create")]
+        [ValidateAntiForgeryToken]
+		public ActionResult Create(Task task)
+		{
+            if (this.ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+
+            return View(task);
+        }
+
+		[HttpGet]
+		[Route("delete/{id}")]
+        public ActionResult Delete(int id)
+		{
+            var task = db.Tasks.Find(id);
+            if (task == null)
+                return HttpNotFound();
+
+            return View(task);
+        }
+
+		[HttpPost]
+		[Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirm(int id)
+		{
+            var taskFromDb = db.Tasks.Find(id);
+            if (taskFromDb == null)
+                return HttpNotFound();
+
+            db.Tasks.Remove(taskFromDb);
+            db.SaveChanges();
+            return Redirect("/");
+        }
+	}
+}
---
+++ Software Technologies/Software Technologies Exams/TODOList/Java Skeleton/src/main/java/todolist/controller/TaskController.java
@@ -0,0 +1,77 @@
+package todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import todolist.bindingModel.TaskBindingModel;
+import todolist.entity.Task;
+import todolist.repository.TaskRepository;
+
+import java.util.List;
+
+@Controller
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Task> tasks =  this.taskRepository.findAll();
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("view", "task/index");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+            model.addAttribute("task", new TaskBindingModel());
+            model.addAttribute("view", "task/create");
+            return "base-layout";
+    }
+
+    @PostMapping("/create")
+    public String createProcess(Model model, TaskBindingModel taskBindingModel, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("message", "Invalid data.");
+            model.addAttribute("task", taskBindingModel);
+            model.addAttribute("view", "task/create");
+            return "base-layout";
+        }
+            Task t = new Task();
+            t.setComments(taskBindingModel.getComments());
+            t.setTitle(taskBindingModel.getTitle());
+            taskRepository.saveAndFlush(t);
+            return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        Task task = this.taskRepository.findOne(id);
+        if (task == null)
+            return "redirect:/";
+
+        model.addAttribute("task", task);
+        model.addAttribute("view", "task/delete");
+        return "base-layout";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProcess(Model model, @PathVariable int id) {
+        Task task = this.taskRepository.findOne(id);
+        if (task == null)
+            return "redirect:/";
+
+        taskRepository.delete(task);
+        taskRepository.flush();
+        return "redirect:/";
+    }
+}
